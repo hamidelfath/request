c1a5a4670ee2ea235df8d3a2d3f5090588056048
hamidelfath <hamidelfath@gmail.com>
2023-04-16 21:02:37 +0700

    upload lagi

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 load('generate_group.sage')
 load('poly_function.py')
 
-n = input('enter the degree:')
+n = int(input('enter the degree:'))
 
-
+for i in range(n):
+    [f1,f2,V] = ff(i)
+    Mat1 = mat_poly(f1,f2,V,M1)
+    Mat2 = mat_poly(f1,f2,V,M2)
+    
+    MM = block_matrix([[Mat1],[Mat2]],subdivide=False)
+    if len(V)>MM.rank():
+        print(i,len(V),MM.rank())
